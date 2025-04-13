b3028c96a19fab8241018922a56313fb793f7f00
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-04-14 05:03:33 +0800

    components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,11 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel Job Board</title>
         @vite('resources/css/app.css')
     </head>
-    <body>
-        <div class="text-5xl">This should be a big text</div>
+    <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+        {{$slot}}
     </body>
 </html>
